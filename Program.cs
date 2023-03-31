15cf6794669599f66069aa0c99a9054097a92a6a
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-31 18:35:14 +0200

    bloccato linea 8 program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine();
+﻿using net_ef_videogame;
+
+using (VideogameContext db = new VideogameContext())
+{
+    //create
+    Videogame videogameProva = new Videogame { Name = "Fifa23", Overview = "Bellissimo" };
+    db.Videogames.Add(videogameProva);
+    var ciao = db.SaveChanges();
+    Console.WriteLine(ciao);
+}
--- Videogame.cs
+++ Videogame.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,5 +19,16 @@ namespace net_ef_videogame
 
         public long SoftwareHouseId { get; set; }
         public SoftwareHouse? SoftwareHouse { get; set; }
+
+
+        public override string ToString()
+        {
+            var ln = Environment.NewLine;
+
+            return $"ID: {Id}" + ln +
+                $"Name: {Name}" + ln +
+                $"Commento: {Overview}" + ln;
+        }
     }
+
 }
